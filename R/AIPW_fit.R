140e2e9e6acba56d72bf433aebdd1e019286a6c4
Jooyoung LEE <joolee0918@gmail.com>
2019-05-28 13:52:20 -0400

    Rstudio

--- R/AIPW_fit.R
+++ R/AIPW_fit.R
@@ -696,6 +696,13 @@ AIPWsubtype <- function(formula, data, id, missing_model, missing_indep = FALSE,
                 warning(msg) else stop(msg)
         }
 
+        print(dim(Sgam))
+        print(dim(Igam))
+        print(dim(fit$Ithegam))
+        print(dim(Salp))
+        print(dim(Ialp))
+        print(dim(fit$Ithealp))
+        print(dim(resid))
         resid = resid - as.matrix(Sgam) %*% Igam %*% t(fit$Ithegam) - as.matrix(Salp) %*% Ialp %*% t(fit$Ithealp)
 
         fit$var <- fit$naive.var %*% t(resid) %*% resid %*% fit$naive.var
