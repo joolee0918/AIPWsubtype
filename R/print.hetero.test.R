a443ab8481625e484c0dd77c974fbd4b2c3eb4ec
Jooyoung LEE <joolee0918@gmail.com>
2019-05-30 23:23:32 -0400

    print

--- R/print.hetero.test.R
+++ R/print.hetero.test.R
@@ -6,12 +6,13 @@
 print.hetero.test <- function(object){
 
   n <- length(object)
+  cat("\n")
   cat("Heterogeneity Tests (Likelihood ratio test) ")
   cat("\n")
   for(i in 1:n){
     cat("---------------------------------------------")
     cat("\n")
     print(object[[i]], row.names = F)
-
   }
+  cat("\n")
 }
